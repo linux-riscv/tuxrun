02535f7f7c05fafa3e2c56ae354f46ae896c0748
Antonio Terceiro <antonio.terceiro@linaro.org>
2021-04-08 18:55:21 -0300

    Ignore empty line coming from lava-run
    Fixes: https://gitlab.com/Linaro/tuxrun/-/issues/1

--- test/test_main.py
+++ test/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+import yaml
 
 import tuxrun.__main__
 from tuxrun.__main__ import start, main
@@ -60,3 +61,28 @@ def test_almost_real_run(tuxrun_args, lava_run, capsys):
     assert exitcode == 0
     stdout, _ = capsys.readouterr()
     assert "Hello, world" in stdout
+
+
+def test_ignores_empty_line_from_lava_run_stdout(tuxrun_args, lava_run):
+    lava_run.stderr = [
+        '{"lvl": "info", "msg": "Hello, world", "dt": "2021-04-08T18:42:25.139513"}\n',
+        "",
+        '{"lvl": "info", "msg": "Hello, world", "dt": "2021-04-08T18:42:26.139513"}\n',
+    ]
+    exitcode = main()
+    assert exitcode == 0
+
+
+def test_ignores_empty_line_from_lava_run_logfile(tuxrun_args, lava_run, tmp_path):
+    log = tmp_path / "log.yaml"
+    tuxrun_args += ["--log", str(log)]
+    lava_run.stderr = [
+        '{"lvl": "info", "msg": "Hello, world", "dt": "2021-04-08T18:42:25.139513"}\n',
+        "",
+        '{"lvl": "info", "msg": "Hello, world", "dt": "2021-04-08T18:42:26.139513"}\n',
+    ]
+    exitcode = main()
+    assert exitcode == 0
+    logdata = yaml.safe_load(log.open())
+    assert type(logdata[0]) is dict
+    assert type(logdata[1]) is dict
