116d9e821731576c72905606dad27f058527bcf6
Rémi Duraffort <remi.duraffort@linaro.org>
2021-05-11 09:59:51 +0200

    template: use trim_blocks to improve output

--- tuxrun/templates/__init__.py
+++ tuxrun/templates/__init__.py
@@ -7,10 +7,12 @@ BASE = (Path(__file__) / "..").resolve()
 
 jobs = jinja2.Environment(
     autoescape=False,
+    trim_blocks=True,
     loader=jinja2.FileSystemLoader(str(BASE / "jobs")),
 )
 
 devices = jinja2.Environment(
     autoescape=False,
+    trim_blocks=True,
     loader=jinja2.FileSystemLoader(str(BASE / "devices")),
 )
