6f56c99256c48d9a2740ba0e215b06ffe681eed9
Antonio Terceiro <antonio.terceiro@linaro.org>
2021-10-01 15:34:57 -0300

    test_results: modularize test data generation

--- test/unit/test_results.py
+++ test/unit/test_results.py
@@ -6,15 +6,19 @@ def test_returns_0_by_default():
     assert results.ret() == 0
 
 
+def gen_test(name, result, suite_name="mytestsuite"):
+    return f'{{ "lvl": "results", "msg": {{"definition": "{suite_name}", "case": "{name}", "result": "{result}"}}}}'
+
+
 def test_returns_0_with_no_failures():
     results = Results()
-    results.parse('{ "lvl": "results", "msg": {"case": "test1", "result": "pass"}}')
-    results.parse('{ "lvl": "results", "msg": {"case": "test2", "result": "pass"}}')
+    results.parse(gen_test("test1", "pass"))
+    results.parse(gen_test("test2", "pass"))
     assert results.ret() == 0
 
 
 def test_returns_1_on_failure():
     results = Results()
-    results.parse('{ "lvl": "results", "msg": {"case": "test1", "result": "pass"}}')
-    results.parse('{ "lvl": "results", "msg": {"case": "test2", "result": "fail"}}')
+    results.parse(gen_test("test1", "pass"))
+    results.parse(gen_test("test2", "fail"))
     assert results.ret() == 1
