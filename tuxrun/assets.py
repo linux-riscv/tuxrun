5791384818fde57236fb82ed84d0fa90760537e8
Anders Roxell <anders.roxell@linaro.org>
2022-11-28 15:08:41 +0100

    asserts: update to latest test-definitions tarball 2022.11
    The new test-definitions will,
    - improve kunit parsing
    - add support for kselftest sharding
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- tuxrun/assets.py
+++ tuxrun/assets.py
@@ -17,7 +17,7 @@ from tuxrun.utils import ProgressIndicator, NoProgressIndicator
 from tuxrun.xdg import get_cache_dir
 
 
-TEST_DEFINITIONS = "https://storage.tuxboot.com/test-definitions/2022.10.tar.zst"
+TEST_DEFINITIONS = "https://storage.tuxboot.com/test-definitions/2022.11.tar.zst"
 
 
 def get_rootfs(
