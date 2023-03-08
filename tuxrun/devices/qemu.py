linux-riscv/tuxrun

commit ad9ea070f5ca84f03f9c82f02f764cf095a38624

devices: qemu: arm64: enable mte
Enable 'mte=on' in the machine variable.

Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

tuxrun/devices/qemu.py
@@ -134,7 +134,7 @@ class QemuArm64(QemuDevice):
 
     arch = "arm64"
     lava_arch = "arm64"
-    machine = "virt,gic-version=3"
+    machine = "virt,gic-version=3,mte=on"
     cpu = "max,pauth-impdef=on"
 
     extra_options = ["-smp 2"]
@@ -179,7 +179,7 @@ class QemuArmv7(QemuDevice):
 
     arch = "armv7"
     lava_arch = "arm"
-    machine = "virt,gic-version=3"
+    machine = "virt,gic-version=3,mte=on"
     cpu = "cortex-a15"
 
     extra_options = ["-smp 2"]

test/unit/refs/definitions/qemu-armv7-kernel-xz.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3"
+    machine: "virt,gic-version=3,mte=on"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7-kselftest-ipc-cpupower.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3"
+    machine: "virt,gic-version=3,mte=on"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7-kselftest-ipc-skipfile.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3"
+    machine: "virt,gic-version=3,mte=on"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7-kselftest-ipc.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3"
+    machine: "virt,gic-version=3,mte=on"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7-ltp-mutliple-tests.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3"
+    machine: "virt,gic-version=3,mte=on"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7-ltp-timeouts.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3"
+    machine: "virt,gic-version=3,mte=on"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3"
+    machine: "virt,gic-version=3,mte=on"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

commit 16ae4625c459dff3f77e9db41fe8a7a98b012566

devices: qemu: armv7: drop mte
Fix bug that was introduced by commit
ad9ea070f5ca ("devices: qemu: arm64: enable mte")
Should only have been enabled for arm64.

Fixes: ad9ea070f5ca ("devices: qemu: arm64: enable mte")
Reported-by: Vishal Bhoj <vishal.bhoj@linaro.org>
Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

tuxrun/devices/qemu.py
@@ -179,7 +179,7 @@ class QemuArmv7(QemuDevice):
 
     arch = "armv7"
     lava_arch = "arm"
-    machine = "virt,gic-version=3,mte=on"
+    machine = "virt,gic-version=3"
     cpu = "cortex-a15"
 
     extra_options = ["-smp 2"]

test/unit/refs/definitions/qemu-armv7-kernel-xz.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3,mte=on"
+    machine: "virt,gic-version=3"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7-kselftest-ipc-cpupower.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3,mte=on"
+    machine: "virt,gic-version=3"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7-kselftest-ipc-skipfile.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3,mte=on"
+    machine: "virt,gic-version=3"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7-kselftest-ipc.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3,mte=on"
+    machine: "virt,gic-version=3"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7-ltp-mutliple-tests.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3,mte=on"
+    machine: "virt,gic-version=3"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7-ltp-timeouts.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3,mte=on"
+    machine: "virt,gic-version=3"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3,mte=on"
+    machine: "virt,gic-version=3"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7be-prompt.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3,mte=on"
+    machine: "virt,gic-version=3"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7be-qemu-image.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3,mte=on"
+    machine: "virt,gic-version=3"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]

test/unit/refs/definitions/qemu-armv7be.yaml
@@ -6,7 +6,7 @@ visibility: "public"
 
 context:
     arch: "arm"
-    machine: "virt,gic-version=3,mte=on"
+    machine: "virt,gic-version=3"
     cpu: "cortex-a15"
     memory: "4G"
     extra_options: ["-no-reboot", "-smp 2"]