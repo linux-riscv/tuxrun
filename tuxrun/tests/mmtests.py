de51b3ac107f6c97855008bf364369e439e00b48
Anders Roxell <anders.roxell@linaro.org>
2023-12-04 16:26:52 +0100

    tests: mmtests: don't use re.sub()
    Suggested-by: Remi Duraffort <remi.duraffort@linaro.org>
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- tuxrun/tests/mmtests.py
+++ tuxrun/tests/mmtests.py
@@ -5,7 +5,6 @@
 # SPDX-License-Identifier: MIT
 
 from tuxrun.tests import Test
-import re
 
 
 class MMTests(Test):
@@ -24,160 +23,160 @@ class MMTests(Test):
 
 class MMTestsDbSqliteInsertSmall(MMTests):
     configfile = "configs/config-db-sqlite-insert-small"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-db-sqlite-insert-small"
     iterations = 10
     timeout = 90
 
 
 class MMTestsHpcScimarkcSmall(MMTests):
     configfile = "configs/config-hpc-scimarkc-small"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-hpc-scimarkc-small"
     iterations = 20
     timeout = 90
 
 
 class MMTestsBlogbench(MMTests):
     configfile = "configs/config-io-blogbench"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-io-blogbench"
     iterations = 30
     timeout = 90
 
 
 class MMTestsFioRandreadAsyncRandwrite(MMTests):
     configfile = "configs/config-io-fio-randread-async-randwrite"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-io-fio-randread-async-randwrite"
     iterations = 10
     timeout = 90
 
 
 class MMTestsFioRandreadAsyncSeqwrite(MMTests):
     configfile = "configs/config-io-fio-randread-async-seqwrite"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-io-fio-randread-async-seqwrite"
     iterations = 10
     timeout = 90
 
 
 class MMTestsFioRandreadSyncHeavywrite(MMTests):
     configfile = "configs/config-io-fio-randread-sync-heavywrite"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-io-fio-randread-sync-heavywrite"
     iterations = 10
     timeout = 90
 
 
 class MMTestsFioRandreadSyncRandwrite(MMTests):
     configfile = "configs/config-io-fio-randread-sync-randwrite"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-io-fio-randread-sync-randwrite"
     iterations = 10
     timeout = 90
 
 
 class MMTestsFsmarkSmallFileStream(MMTests):
     configfile = "configs/config-io-fsmark-small-file-stream"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-io-fsmark-small-file-stream"
     iterations = 10
     timeout = 90
 
 
 class MMTestsRedisBenchmarkSmall(MMTests):
     configfile = "configs/config-memdb-redis-benchmark-small"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-memdb-redis-benchmark-small"
     iterations = 20
     timeout = 90
 
 
 class MMTestsRedisMemtierSmall(MMTests):
     configfile = "configs/config-memdb-redis-memtier-small"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-memdb-redis-memtier-small"
     iterations = 20
     timeout = 90
 
 
 class MMTestsSchbench(MMTests):
     configfile = "configs/config-scheduler-schbench"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-scheduler-schbench"
     iterations = 10
     timeout = 90
 
 
 class MMTestsSysbenchCpu(MMTests):
     configfile = "configs/config-scheduler-sysbench-cpu"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-scheduler-sysbench-cpu"
     iterations = 10
     timeout = 90
 
 
 class MMTestsSysbenchThread(MMTests):
     configfile = "configs/config-scheduler-sysbench-thread"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-scheduler-sysbench-thread"
     iterations = 10
     timeout = 90
 
 
 class MMTestsCoremark(MMTests):
     configfile = "configs/config-workload-coremark"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-workload-coremark"
     iterations = 20
     timeout = 90
 
 
 class MMTestsCyclictestFineHackbench(MMTests):
     configfile = "configs/config-workload-cyclictest-fine-hackbench"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-workload-cyclictest-fine-hackbench"
     iterations = 15
     timeout = 90
 
 
 class MMTestsCyclictestHackbench(MMTests):
     configfile = "configs/config-workload-cyclictest-hackbench"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-workload-cyclictest-hackbench"
     iterations = 20
     timeout = 90
 
 
 class MMTestsEbizzy(MMTests):
     configfile = "configs/config-workload-ebizzy"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-workload-ebizzy"
     iterations = 10
     timeout = 90
 
 
 class MMTestsPmqtestHackbench(MMTests):
     configfile = "configs/config-workload-pmqtest-hackbench"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-workload-pmqtest-hackbench"
     iterations = 10
     timeout = 90
 
 
 class MMTestsStressngClassIoParallel(MMTests):
     configfile = "configs/config-workload-stressng-class-io-parallel"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-workload-stressng-class-io-parallel"
     iterations = 10
     timeout = 90
 
 
 class MMTestsStressngContext(MMTests):
     configfile = "configs/config-workload-stressng-context"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-workload-stressng-context"
     iterations = 10
     timeout = 90
 
 
 class MMTestsStressngGet(MMTests):
     configfile = "configs/config-workload-stressng-get"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-workload-stressng-get"
     iterations = 10
     timeout = 90
 
 
 class MMTestsStressngMmap(MMTests):
     configfile = "configs/config-workload-stressng-mmap"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-workload-stressng-mmap"
     iterations = 10
     timeout = 90
 
 
 class MMTestsUsemem(MMTests):
     configfile = "configs/config-workload-usemem"
-    name = re.sub(r"configs/config-", "mmtests-", configfile)
+    name = "mmtests-workload-usemem"
     iterations = 10
     timeout = 90
