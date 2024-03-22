8d97c0ab6f85bbe98dab2b39bc07eae81e324b3c
Anders Roxell <anders.roxell@linaro.org>
2024-03-22 07:10:59 +0100

    assert: test-definitions: update to tag 2024.03
    Update to test-definitions release 2024.03 from 2024.02.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- tuxrun/assets.py
+++ tuxrun/assets.py
@@ -16,7 +16,7 @@ from tuxrun.requests import requests_get
 from tuxrun.utils import NoProgressIndicator, ProgressIndicator, pathurlnone
 from tuxrun.xdg import get_cache_dir
 
-TEST_DEFINITIONS = "https://storage.tuxboot.com/test-definitions/2024.02.tar.zst"
+TEST_DEFINITIONS = "https://storage.tuxboot.com/test-definitions/2024.03.tar.zst"
 
 
 def get_rootfs(
